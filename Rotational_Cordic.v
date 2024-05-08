3b48edb6d08bee2e1b546acaeeafa0d57a9c0e7e
OmarMahmoudElsherif <104269847+OmarMahmoudElsherif@users.noreply.github.com>
2024-05-09 00:19:16 +0300

    Add files via upload

--- Rotational_Cordic.v
+++ Rotational_Cordic.v
@@ -1,14 +1,14 @@
 module Rotational_Cordic #( 
-	parameter 	INT_LENGTH			=	6, 
-	parameter	FRAC_LENGTH			=	12,
-	parameter 	NUM_OF_ITERATIONS	=	12
+	parameter 	INT_LENGTH				=	7, 
+	parameter		FRAC_LENGTH				=	11,
+	parameter 	NUM_OF_ITERATIONS	=	11
 )  (
 
 ///////////////////// Inputs /////////////////////////////////
 
-input  wire                                          CLK,
-input  wire                                          RST,
-input  wire                                          ENABLE,
+input  wire                                        CLK,
+input  wire                                        RST,
+input  wire                                        ENABLE,
 input  wire signed  [INT_LENGTH+FRAC_LENGTH-1:0]	 Xo,
 input  wire signed  [INT_LENGTH+FRAC_LENGTH-1:0]	 Yo,
 input  wire signed  [INT_LENGTH+FRAC_LENGTH-1:0]	 Zo,
@@ -17,8 +17,7 @@ input  wire signed  [INT_LENGTH+FRAC_LENGTH-1:0]	 Zo,
 
 output reg 	signed  [INT_LENGTH+FRAC_LENGTH-1:0]	 XN,
 output reg 	signed  [INT_LENGTH+FRAC_LENGTH-1:0]	 YN,
-output reg signed  [INT_LENGTH+FRAC_LENGTH-1:0]	 ZN,
-output reg                                          Done
+output reg                                         Done
 
 );
 
@@ -31,18 +30,16 @@ output reg                                          Done
 
 localparam WORD_LENGTH = INT_LENGTH + FRAC_LENGTH ;
 
-// Fixed Variables from Matlab
-// vary according to wordlength
-// Example with wordlength 18 : 12 - 6 
-wire signed [WORD_LENGTH-1:0] two_pi                = 'h06488 ;
-wire signed [WORD_LENGTH-1:0] minus_two_pi          = 'h39b78 ;
-wire signed [WORD_LENGTH-1:0] pi                    = 'h03244 ;
-wire signed [WORD_LENGTH-1:0] minus_pi              = 'h3cdbc ;
-wire signed [WORD_LENGTH-1:0] half_pi               = 'h01922 ;
-wire signed [WORD_LENGTH-1:0] minus_half_pi         = 'h3e6de ;
-wire signed [WORD_LENGTH-1:0] three_pi_over_2       = 'h04b66 ;
-wire signed [WORD_LENGTH-1:0] minus_three_pi_over_2 = 'h3b49a ;
-wire signed [WORD_LENGTH-1:0] Scaling               = 'h009b7 ;
+// Fixed Variables from Matlab, they Vary according to wordlength
+// WL = 18 , FL = 11
+wire signed [WORD_LENGTH-1:0] two_pi                = 'h03243 ;
+wire signed [WORD_LENGTH-1:0] minus_two_pi          = 'h3cdbc ;
+wire signed [WORD_LENGTH-1:0] pi                    = 'h01921 ;
+wire signed [WORD_LENGTH-1:0] half_pi               = 'h00c90 ;
+wire signed [WORD_LENGTH-1:0] minus_half_pi         = 'h3f36f ;
+wire signed [WORD_LENGTH-1:0] three_pi_over_2       = 'h025b2 ;
+wire signed [WORD_LENGTH-1:0] minus_three_pi_over_2 = 'h3da4d ;
+wire signed [WORD_LENGTH-1:0] Kn               			= 'h004db ;
 
 
 //////////////////////////////////////////////////////////////
@@ -50,20 +47,18 @@ wire signed [WORD_LENGTH-1:0] Scaling               = 'h009b7 ;
 //////////////////////////////////////////////////////////////
 
 
-// Store Zo Input
-reg  signed [WORD_LENGTH-1:0] z_n ;
 // Registers for temporary x and y and z (overwritten each iteration)
-reg signed [WORD_LENGTH-1:0] x_n_reg ;
-reg signed [WORD_LENGTH-1:0] y_n_reg ;
-reg signed [WORD_LENGTH-1:0] z_n_reg ;
+reg 	signed [WORD_LENGTH-1:0] 	x_n_reg ;
+reg 	signed [WORD_LENGTH-1:0] 	y_n_reg ;
+reg 	signed [WORD_LENGTH-1:0] 	z_n_reg ;
 // Temporary outputs Scaled before truncation      
-wire signed [2*WORD_LENGTH-1:0] XN_double  ;
-wire signed [2*WORD_LENGTH-1:0] YN_double  ;
+wire 	signed [2*WORD_LENGTH-1:0] XN_double  ;
+wire 	signed [2*WORD_LENGTH-1:0] YN_double  ;
 // Shifted Variables
-wire signed	[WORD_LENGTH-1:0] x_shifted ;
-wire signed	[WORD_LENGTH-1:0] y_shifted ;
+wire 	signed	[WORD_LENGTH-1:0] x_shifted ;
+wire 	signed	[WORD_LENGTH-1:0] y_shifted ;
 // Counter number of iterations
-reg [$clog2(NUM_OF_ITERATIONS):0] Count_iterations_reg;
+reg 	[$clog2(NUM_OF_ITERATIONS):0] Count_iterations_reg;
 
 // enable flag
 reg flag_reg;
@@ -77,7 +72,7 @@ reg flag_reg;
 
 
 
-// LUT of arctan *For WL=18*
+// LUT of arctan *For WL=18, FL=11*
 reg signed  [WORD_LENGTH-1:0]    arctan_LUT    [NUM_OF_ITERATIONS - 1:0];
 integer i;
 
@@ -91,55 +86,20 @@ always @(posedge CLK or negedge RST) begin
     	end 
 	end
 	else begin
-		arctan_LUT[0]  <= 'b000000110010010000 ;
-		arctan_LUT[1]  <= 'b000000011101101011 ;
-		arctan_LUT[2]  <= 'b000000001111101011 ;
-		arctan_LUT[3]  <= 'b000000000111111101 ;
-		arctan_LUT[4]  <= 'b000000000011111111 ;
-		arctan_LUT[5]  <= 'b000000000001111111 ;
-		arctan_LUT[6]  <= 'b000000000000111111 ;
-		arctan_LUT[7]  <= 'b000000000000011111 ;
-		arctan_LUT[8]  <= 'b000000000000001111 ;
-		arctan_LUT[9]  <= 'b000000000000000111 ;
-		arctan_LUT[10] <= 'b000000000000000011 ;
-		arctan_LUT[11] <= 'b000000000000000001 ;
+		arctan_LUT[0]  <= 'b000000011001001000 ;
+		arctan_LUT[1]  <= 'b000000001110110101 ;
+		arctan_LUT[2]  <= 'b000000000111110101 ;
+		arctan_LUT[3]  <= 'b000000000011111110 ;
+		arctan_LUT[4]  <= 'b000000000001111111 ;
+		arctan_LUT[5]  <= 'b000000000000111111 ;
+		arctan_LUT[6]  <= 'b000000000000011111 ;
+		arctan_LUT[7]  <= 'b000000000000001111 ;
+		arctan_LUT[8]  <= 'b000000000000000111 ;
+		arctan_LUT[9]  <= 'b000000000000000011 ;
+		arctan_LUT[10] <= 'b000000000000000001 ;
 	end
 end
 
-
-
-
-//////////////////////////////////////////////////////////////
-/////////////////  Quadrant Correction  //////////////////////
-//////////////////////////////////////////////////////////////
-
-
-always@(*)begin
-
-     if(Zo	>=	two_pi)    begin
-      	z_n = Zo - two_pi ;
-      end        
-      else if(Zo <= minus_two_pi) begin
-      	z_n = Zo + two_pi ;
-      end
-	  else if(Zo > (minus_two_pi) && Zo < (minus_three_pi_over_2))	begin
-      	z_n = Zo + two_pi;
-	  end
-	  else if(Zo >= (minus_three_pi_over_2) && Zo < (minus_half_pi)) begin
-	  	z_n = Zo + pi;
-	  end
-	  else if(Zo > (half_pi) && Zo <= (three_pi_over_2)) begin
-		z_n = Zo - pi;       
-	  end
-	  else if(Zo > (three_pi_over_2) && Zo <= (two_pi)) begin
-		z_n = Zo - two_pi;            
-	  end
-	  //else if(Zo >= (minus_half_pi) && Zo <= (half_pi)) begin
-	  else begin 
-	    z_n = Zo;
-	  end
-	    
-end
 
 
 
@@ -155,7 +115,7 @@ always @(posedge CLK or negedge RST)
 		y_n_reg		<= 'b0;
 		z_n_reg 	<= 'b0;
 		flag_reg	<=	'b0;
-		Done         <=  'b0;
+		Done        <=  'b0;
 		Count_iterations_reg <= 0;
 	end
 
@@ -164,13 +124,38 @@ always @(posedge CLK or negedge RST)
 		if(ENABLE) begin
 			x_n_reg 	<= 	Xo;
 			y_n_reg 	<= 	Yo;
-			z_n_reg 	<= 	z_n;
 			flag_reg	<=	'b1;
+			Done        <=  'b0;
 			Count_iterations_reg <= 'b0;
+			
+			/*z_n_reg initial value will depend on the quadrant of the input angle*/
+      if(Zo	>=	two_pi)    begin
+      	z_n_reg = Zo - two_pi ;
+      end        
+      else if(Zo <= minus_two_pi) begin
+      	z_n_reg = Zo + two_pi ;
+      end
+      else if(Zo >= (half_pi) && Zo <= (three_pi_over_2)) begin
+      	z_n_reg = Zo - pi;       
+      end
+      else if(Zo > (three_pi_over_2) && Zo <= (two_pi)) begin
+      	z_n_reg = Zo - two_pi;            
+      end
+      else if(Zo >= (minus_three_pi_over_2) && Zo <= (minus_half_pi)) begin
+      	z_n_reg = Zo + pi;
+      end
+      else if(Zo >= (minus_two_pi) && Zo < (minus_three_pi_over_2))	begin
+      	z_n_reg = Zo + two_pi;
+      end
+      else begin 
+      	z_n_reg = Zo;
+      end
+                  
 		end
 
-	   	else if( flag_reg == 'b1 ) begin
 
+	  else if( flag_reg == 'b1 ) begin  // flag_reg is high aslong as iterations are counting
+               
 			if (z_n_reg[WORD_LENGTH-1])  begin
 			      x_n_reg <= x_n_reg + y_shifted ;
 			      y_n_reg <= y_n_reg - x_shifted ;
@@ -182,38 +167,37 @@ always @(posedge CLK or negedge RST)
 			      z_n_reg <= z_n_reg - arctan_LUT[Count_iterations_reg] ;		   	
 			end
 
-			if(Count_iterations_reg	==	(NUM_OF_ITERATIONS-1)) begin
-			    flag_reg	<=	'b0;
-			    Done         <=  'b1;
+			if(Count_iterations_reg	==	(NUM_OF_ITERATIONS)) begin
+			    flag_reg	 <=	'b0;
+			    Done         <= 'b1;    // Done is high when iterations are finished,and at that time you can take the correct output
+			    z_n_reg      <= 'b0;
 			end
 			else begin
-			    Count_iterations_reg <= Count_iterations_reg + 1'b1 ;
-			    Done         <=  'b0;
+			    Count_iterations_reg <=  Count_iterations_reg + 1'b1 ;
+			    Done                 <=  'b0;
+			    flag_reg	         <=	 'b1;
 			end
-	    end
+	  end
 
-	    else begin
+	  else begin
 			flag_reg	<=	'b0;
 			x_n_reg 	<= 	'b0;
 			y_n_reg		<= 	'b0;
 			z_n_reg 	<= 	'b0;
-			Done         <=  'b0;
+			Done        <=  'b0;
 			Count_iterations_reg	<=	'b0;
 			
-	    end
+	  end
 	     	
 	end
 end
 
-
 //////////////////////////////////////////////////////////////
-//////////////////////  Done Signal  /////////////////////////
+///////////////  Cosntant Output  Flag ///////////////////////
 //////////////////////////////////////////////////////////////
 
-// Done Signal
-//assign Done = ( Count_iterations_reg == (NUM_OF_ITERATIONS-1) && !flag_reg ) ?  1'b1 : 1'b0 ;
-
-
+//This flag is responsible to make the output bus steady on the value it reached when counter reached max no. of iterations
+assign before_end=(Count_iterations_reg==(NUM_OF_ITERATIONS))? 1'b1:1'b0;
 
 //////////////////////////////////////////////////////////////
 ///////////////  Shifting CORDIC Operands  ///////////////////
@@ -228,36 +212,43 @@ assign y_shifted = y_n_reg >>> Count_iterations_reg;
 //////  Outputs Scalling and Quadrants Sign Correction  //////
 //////////////////////////////////////////////////////////////
 
-// After Scaling
-assign XN_double = x_n_reg * Scaling ;
-assign YN_double = y_n_reg * Scaling ;
+// After Kn
+assign XN_double = x_n_reg * Kn ;
+assign YN_double = y_n_reg * Kn ;
+
+
 
 
 // Correction of Output Sign of Yn , Xn
+// Output is zero until done comes then output is steady as this value till new enable comes
 always@(posedge CLK,negedge RST)begin
     if(!RST) begin
         XN <= 'b0;
         YN <= 'b0;
-        ZN <= 'b0;
     end
-	else begin
-	    ZN <= z_n_reg;
-	    if(Zo >= (minus_three_pi_over_2) && Zo < (minus_half_pi))
+	else if(before_end && !Done)  begin
+	
+	    if(Zo >= (half_pi) && Zo <= (three_pi_over_2))
 	    begin
 	    	XN <= - XN_double[WORD_LENGTH+FRAC_LENGTH-1:FRAC_LENGTH];
 	    	YN <= - YN_double[WORD_LENGTH+FRAC_LENGTH-1:FRAC_LENGTH];
 	    end
-	    else if(Zo > (half_pi) && Zo <= (three_pi_over_2))
+	    else if(Zo <= (two_pi) && Zo > (half_pi))
 	    begin
-	    	XN <= - XN_double[WORD_LENGTH+FRAC_LENGTH-1:FRAC_LENGTH];
-	    	YN <= - YN_double[WORD_LENGTH+FRAC_LENGTH-1:FRAC_LENGTH];		         
+	    	XN <=   XN_double[WORD_LENGTH+FRAC_LENGTH-1:FRAC_LENGTH];
+	    	YN <=   YN_double[WORD_LENGTH+FRAC_LENGTH-1:FRAC_LENGTH];		         
 	    end
+	    else if(Zo <= (minus_half_pi) && Zo >= (minus_three_pi_over_2)) begin
+	    	XN <= - XN_double[WORD_LENGTH+FRAC_LENGTH-1:FRAC_LENGTH];
+	    	YN <= - YN_double[WORD_LENGTH+FRAC_LENGTH-1:FRAC_LENGTH];	
+        end
 	    else begin
 	    	XN <= XN_double[WORD_LENGTH+FRAC_LENGTH-1:FRAC_LENGTH];
 	    	YN <= YN_double[WORD_LENGTH+FRAC_LENGTH-1:FRAC_LENGTH];	         	
 	    end
 	 end
 end
+
 
 
 
